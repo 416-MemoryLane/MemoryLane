2d6e77008bfb82920405642c6bcfd30498fac930
eysk-io <erickim195@gmail.com>
2023-02-15 22:47:58 -0800

    Add CreateAlbum sample

--- app.go
+++ app.go
@@ -15,6 +15,13 @@ func main() {
 		l.Fatal("Error initializing gallery: ", err)
 	}
 
+	// a, err := g.CreateAlbum("cool-album4")
+	// if err != nil {
+	// 	l.Println("Error creating album:", err)
+	// } else {
+	// 	l.Printf(a)
+	// }
+
 	l.Printf("%v", g)
 
 	// Most likely will want to reconcile gallery with other nodes here
