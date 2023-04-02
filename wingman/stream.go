f893a684149680ea4237f5e615d3e286af8ff13f
eysk-io <erickim195@gmail.com>
2023-04-02 15:08:21 -0700

    Add cases to handle

--- wingman/stream.go
+++ wingman/stream.go
@@ -18,5 +18,26 @@ func (wh *WingmanHandler) HandleStream(stream network.Stream) {
 		}
 
 		wh.l.Println("received message:", d.Message)
+
+		// if the message received has nothing to reconcile
+		// continue
+
+		// if the message received is missing one or more deletes
+		// continue
+
+		// if the message received has one or more deletes
+		// delete the photos from the filesystem
+		// reconcile CRDT
+
+		// if the message received is missing one or more photos
+		// send a message to this node with the photos it's missing
+
+		// if the message received has one or more photos with all of the missing photos
+		// add the photos to the filesystem
+		// reconcile CRDT
+
+		// if the message received has one or more photos with some of the missing photos
+		// add the photos to the filesystem
+		// reconcile CRDT with only the added photos
 	}
 }
