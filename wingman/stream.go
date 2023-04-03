da8d1e5a5a999605d6c454ab8b1648c6fb66b2ae
eysk-io <erickim195@gmail.com>
2023-04-02 20:23:47 -0700

    Use a for-loop to reconcile album states in handler

--- wingman/stream.go
+++ wingman/stream.go
@@ -27,27 +27,29 @@ func (wh *WingmanHandler) HandleStream(stream network.Stream) {
 		album := (*wh.Gallery.Albums)[msgAlbum]
 		albumCrdt := album.Crdt
 
-		// Album states are equal so there is nothing to reconcile
-		if albumCrdt.Equals(msgCrdt) {
-			continue
+		for {
+			// Album states are equal so there is nothing to reconcile
+			if albumCrdt.Equals(msgCrdt) {
+				break
+			}
+
+			// if the message received is missing one or more deletes
+			// continue
+
+			// if the message received has one or more deletes
+			// delete the photos from the filesystem
+			// reconcile CRDT
+
+			// if the message received is missing one or more photos
+			// send a message to this node with the photos it's missing
+
+			// if the message received has one or more photos with all of the missing photos
+			// add the photos to the filesystem
+			// reconcile CRDT
+
+			// if the message received has one or more photos with some of the missing photos
+			// add the photos to the filesystem
+			// reconcile CRDT with only the added photos
 		}
-
-		// if the message received is missing one or more deletes
-		// continue
-
-		// if the message received has one or more deletes
-		// delete the photos from the filesystem
-		// reconcile CRDT
-
-		// if the message received is missing one or more photos
-		// send a message to this node with the photos it's missing
-
-		// if the message received has one or more photos with all of the missing photos
-		// add the photos to the filesystem
-		// reconcile CRDT
-
-		// if the message received has one or more photos with some of the missing photos
-		// add the photos to the filesystem
-		// reconcile CRDT with only the added photos
 	}
 }
