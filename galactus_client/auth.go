2fc51c8e0bc0dfa289bc3d42198870c6091eb6c6
Eric Kim <erickim195@gmail.com>
2023-04-04 22:48:31 -0700

    Add stubs for auth

---
+++ galactus_client/auth.go
@@ -0,0 +1,40 @@
+package galactus_client
+
+type AuthInfo struct {
+}
+
+// Endpoint used to log authenticate user through Galactus
+// The same endpoint can be used for new and returning users
+// If a new user uses this endpoint to sign up, a new document will be created in the User collection in MongoDB
+// TODO: can this fetch the albums the user has access to, like gc.Sync?
+func (gc *GalactusClient) Login() *AuthInfo {
+	return nil
+}
+
+// Fetches all the albums the user has access to and the multiaddrs of all the peers who also have access to each album
+func (gc *GalactusClient) Sync() *AuthInfo {
+	return nil
+}
+
+// Creates an album and saves it to MongoDB. Must also send a list of authorized users with payload
+func (gc *GalactusClient) AddAlbum() *AuthInfo {
+	return nil
+}
+
+// Updates the authenticated users array of an album
+// Token required and must be creator of album to edit
+func (gc *GalactusClient) UpdateAlbum() *AuthInfo {
+	return nil
+}
+
+// Retrieves list of all users (username)
+// Token required
+func (gc *GalactusClient) GetUsers() *AuthInfo {
+	return nil
+}
+
+// Delete an album
+// Token required
+func (gc *GalactusClient) DeleteAlbum() *AuthInfo {
+	return nil
+}
