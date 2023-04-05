c5d28503e91fbdabe70ff38861ba25c703c9c8bb
Eric Kim <erickim195@gmail.com>
2023-04-05 13:14:03 -0700

    Adjust error logs

--- galactus_client/client.go
+++ galactus_client/client.go
@@ -41,26 +41,26 @@ func (gc *GalactusClient) Login() (*LoginResponse, error) {
 	url := fmt.Sprintf("%s/login", gc.API)
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
-		return nil, fmt.Errorf("error creating Galactus request: %w", err)
+		return nil, fmt.Errorf("error creating Galactus request to login: %w", err)
 	}
 	req.Header.Add("Content-Type", "application/json")
 
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("error performing HTTP request to Galactus: %w", err)
+		return nil, fmt.Errorf("error performing HTTP request to Galactus while logging in: %w", err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("error reading HTTP response from Galactus: %w", err)
+		return nil, fmt.Errorf("error reading HTTP response from Galactus while logging in: %w", err)
 	}
 
 	// Unmarshal JSON response into struct
 	var loginResp LoginResponse
 	err = json.Unmarshal(body, &loginResp)
 	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling response from Galactus: %w", err)
+		return nil, fmt.Errorf("error unmarshalling response from Galactus while logging in: %w", err)
 	}
 
 	return &loginResp, nil
