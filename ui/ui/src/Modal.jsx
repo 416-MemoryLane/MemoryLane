4e93be1eadf8b54778e0e3bc6dae69715af1f733
Scott King <scott.m.king@outlook.com>
2023-04-06 08:33:56 -0700

    Fix dark mode bug

--- ui/ui/src/Modal.jsx
+++ ui/ui/src/Modal.jsx
@@ -2,15 +2,15 @@ import { CloseIcon } from "./Icons/CloseIcon";
 
 export const Modal = ({ isOpen, children, callbackFn }) => {
   return isOpen ? (
-    <div className="fixed flex h-screen w-screen flex-col content-center items-center justify-center bg-gray-500/50 backdrop-blur-md dark:bg-gray-900/60 z-50">
+    <div className="fixed flex h-screen w-screen flex-col content-center items-center justify-center bg-gray-500/50 backdrop-blur-md z-50">
       <div className={`flex max-h-screen  max-w-[1440px] flex-col`}>
         <div className="fixed z-50 -mr-5 place-self-end ">
           <CloseIcon
-            className="z-50 rounded-full bg-slate-300 hover:cursor-pointer "
+            className="z-50 rounded-full bg-slate-300 hover:cursor-pointer"
             onClick={callbackFn}
           />
         </div>
-        <div className="flex justify-center my-4 rounded-2xl border border-gray-400 bg-white p-10 shadow-2xl dark:bg-slate-800">
+        <div className="flex justify-center my-4 rounded-2xl border border-gray-400 bg-white p-10 shadow-2xl">
           {children}
         </div>
       </div>
