45992a75e24317b20fe19d09e0c1b58bb1dbb355
Scott King <scott.m.king@outlook.com>
2023-04-06 11:05:19 -0700

    Update powershell script

--- ui/start_ui.ps1
+++ ui/start_ui.ps1
@@ -1,8 +1,10 @@
-cd .\ui
+
+Copy-Item -Path "..\.env" -Destination ".\server\.env"
+Set-Location -Path ".\ui"
 yarn
 yarn build
-Remove-Item -Recurse -Force ..\server\dist
-Move-Item .\dist ..\server\dist
-cd ..\server
+Remove-Item -Path "..\server\dist" -Recurse -Force
+Move-Item -Path ".\dist" -Destination "..\server\dist"
+Set-Location -Path "..\server"
 yarn
 yarn start
